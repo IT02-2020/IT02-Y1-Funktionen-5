c28eb09f7eeaf2ac1920420f5575015a425dc0ed
TJ <t0001@outlook.de>
2021-02-10 15:57:35 +0100

    Projektdateien hinzufügen.

---
+++ IT02-Y1-Funktionen-5/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace IT02_Y1_Funktionen_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.eingabeLabel = new System.Windows.Forms.Label();
+            this.yTextBox = new System.Windows.Forms.TextBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.resultListView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Location = new System.Drawing.Point(119, 37);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(100, 20);
+            this.xTextBox.TabIndex = 0;
+            // 
+            // eingabeLabel
+            // 
+            this.eingabeLabel.AutoSize = true;
+            this.eingabeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eingabeLabel.Location = new System.Drawing.Point(45, 37);
+            this.eingabeLabel.Name = "eingabeLabel";
+            this.eingabeLabel.Size = new System.Drawing.Size(68, 20);
+            this.eingabeLabel.TabIndex = 1;
+            this.eingabeLabel.Text = "Eingabe";
+            // 
+            // yTextBox
+            // 
+            this.yTextBox.Location = new System.Drawing.Point(225, 37);
+            this.yTextBox.Name = "yTextBox";
+            this.yTextBox.Size = new System.Drawing.Size(100, 20);
+            this.yTextBox.TabIndex = 2;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(331, 34);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 3;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // resultListView
+            // 
+            this.resultListView.HideSelection = false;
+            this.resultListView.Location = new System.Drawing.Point(49, 85);
+            this.resultListView.Name = "resultListView";
+            this.resultListView.Size = new System.Drawing.Size(357, 208);
+            this.resultListView.TabIndex = 4;
+            this.resultListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 305);
+            this.Controls.Add(this.resultListView);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.yTextBox);
+            this.Controls.Add(this.eingabeLabel);
+            this.Controls.Add(this.xTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.Label eingabeLabel;
+        private System.Windows.Forms.TextBox yTextBox;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.ListView resultListView;
+    }
+}
+
---
+++ IT02-Y1-Funktionen-5/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IT02_Y1_Funktionen_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public int Add(int x, int y)
+        {
+            
+            if(x ==  0 && y == 0)
+            {
+                return 0;
+            }
+
+            else if (x == 0)
+            {
+                return 1 + Add(x, y - 1);
+            }
+            else if (y == 0)
+            {
+                return 1 + Add(x - 1, y);
+            }
+            else
+            {
+                return 1 + 1 + Add(x - 1, y - 1);
+            }
+        }
+
+
+        public int Sub(int x, int y)
+        {
+            if (y == 0)
+            {
+                return  Add(x,0);
+            }
+            else
+            {
+                return Sub(x, y - 1) - 1;
+            }
+        }
+
+        public int Mul(int x, int y)
+        {
+            if (y == 0)
+            {
+                return 0;
+            }
+            else if (y == 1)
+            {
+                return Add(x, 0);
+            }
+            else
+            {
+                return Add(Add(x,0),Mul(x,y-1));
+            }
+
+        }
+
+        public int Div(int x, int y)
+        {
+            if (Sub(x,y) < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1 + Div(Sub(x, y), y);
+            }
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+
+            if (xTextBox.TextLength > 0 && yTextBox.TextLength > 0)
+            {
+                if (int.TryParse(xTextBox.Text, out int numOne) && int.TryParse(yTextBox.Text, out int numTwo))
+                {
+                    resultListView.Items.Add($"{numOne} + {numTwo} = {Add(numOne, numTwo)}");
+                    resultListView.Items.Add($"{numOne} - {numTwo} = {Sub(numOne, numTwo)}");
+                    resultListView.Items.Add($"{numOne} * {numTwo} = {Mul(numOne, numTwo)}");
+                    resultListView.Items.Add($"{numOne} / {numTwo} = {Div(numOne, numTwo)}");
+                }
+            }
+
+        }
+    }
+}
